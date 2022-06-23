a9590024f88e07aa6ce95b0e203c0a11e02420cd
unknown <sezeranochrisostom123@gmail.com>
2022-06-23 18:00:58 +0200

    ft(setup): home page
    Adds home page
    
    #WIP

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,1 +1,7 @@
 /* eslint-disable no-undef */
+
+const DoList = artifacts.require("DoList");
+
+module.export = async function(deployer) {
+    
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="Home flex items-center justify-center bg-slate-900 min-h-screen">
+        <div className="main text-center text-cyan-500 text-5xl uppercase font-extrabold">
+            My todo dapp in ethereum blockchain
+        </div>
+    </div>
+  );
+}
+
+export default Home;
